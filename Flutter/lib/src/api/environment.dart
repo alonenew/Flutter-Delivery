cd4110ef0fdf19e3099d2e98e5253b1104487814
Sittichai Thammawat <alonenew01@gmail.com>
2022-01-24 23:36:34 +0700

    edit env

--- Flutter/lib/src/api/environment.dart
+++ Flutter/lib/src/api/environment.dart
@@ -1,7 +1,7 @@
 import 'package:ardear_bakery/src/models/mercado_pago_credentials.dart';
 
 class Environment {
-  static const String API_DELIVERY = "192.168.1.100:3000";
+  static const String API_DELIVERY = "ardearbakery.herokuapp.com";
   static const String API_KEY_MAPS = "TU_API_KEY";
 
   static MercadoPagoCredentials mercadoPagoCredentials = MercadoPagoCredentials(
